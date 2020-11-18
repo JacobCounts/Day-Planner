4a2322935600d3b4d6b0d8afff14f44c13fd01bc
Jacob Counts <infamous213@gmail.com>
2020-11-18 08:41:23 -0700

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Day-Planner
+
+## About
+
+With this day planner you can add tasks to your daily schedule
+
+
+### Items to complete 
+
+- save data to local storage 
+
--- script.js
+++ script.js
@@ -4,63 +4,63 @@ var myDay = [
     hour: "9:00",
     time: "09",
     meridiem: "AM",
-    reminder: " ",
+    toDos: " ",
   },
   {
     id: "1",
     hour: "10:00",
     time: "10",
     meridiem: "AM",
-    reminder: " ",
+    toDos: " ",
   },
   {
     id: "2",
     hour: "11:00",
     time: "11",
     meridiem: "AM",
-    reminder: " ",
+    toDos: " ",
   },
   {
     id: "3",
     hour: "12:00",
     time: "12",
     meridiem: "PM",
-    reminder: " ",
+    toDos: " ",
   },
   {
     id: "4",
     hour: "1:00",
     time: "13",
     meridiem: "PM",
-    reminder: " ",
+    toDos: " ",
   },
   {
     id: "5",
     hour: "2:00",
     time: "14",
     meridiem: "PM",
-    reminder: " ",
+    toDos: " ",
   },
   {
     id: "6",
     hour: "3:00",
     time: "15",
     meridiem: "PM",
-    reminder: " ",
+    toDos: " ",
   },
   {
     id: "7",
     hour: "4:00",
     time: "16",
     meridiem: "PM",
-    reminder: " ",
+    toDos: " ",
   },
   {
     id: "8",
     hour: "5:00",
     time: "17",
     meridiem: "PM",
-    reminder: " ",
+    toDos: " ",
   },
 
   {
@@ -68,7 +68,7 @@ var myDay = [
     hour: "6:00",
     time: "18",
     meridiem: "PM",
-    reminder: " ",
+    toDos: " ",
   },
 
   {
@@ -76,41 +76,41 @@ var myDay = [
     hour: "7:00",
     time: "19",
     meridiem: "PM",
-    reminder: " ",
+    toDos: " ",
   },
 ];
-// gets data for the header 
-function getHeaderDate() {
+
+var toDos = [" "];
+
+// Date for the header
+function getDate() {
   var headerDate = moment().format("dddd, MMMM DD");
   $("#currentDay").text(headerDate);
 }
 
-// saves data to localStorage
-function saveReminders() {
+//saves data to localStorage
+function toDos() {
   localStorage.setItem("myDay", JSON.stringify(myDay));
 }
 
-//sets data in localStorage 
-function displayReminders() {
+//sets data in localStorage
+function displaytoDos() {
   myDay.forEach(function (thisHour) {
-    $(`${thisHour.id}`).val(thisHour.reminder);
+    $(`${thisHour.id}`).val(thisHour.toDos);
   });
 }
 
-// sets existing localStorage
+//sets existing localStorage
 function init() {
   var storedDay = JSON.parse(localStorage.getItem("myDay"));
-  
+
   if (storedDay) {
     myDay = storedDay;
   }
-  
-  saveReminders();
-  //displayReminders();
+
 }
 
-// loads header date
-getHeaderDate();
+getDate();
 
 // creates the planner body
 myDay.forEach(function (thisHour) {
@@ -119,35 +119,35 @@ myDay.forEach(function (thisHour) {
     class: "row",
   });
   $(".container").append(hourRow);
-  
+
   // creates time field
   var hourField = $("<div>").text(`${thisHour.hour}${thisHour.meridiem}`).attr({
     class: "col-md-2 hour",
   });
-  
+
   // creates schdeduler data
   var hourPlan = $("<div>").attr({
-    class: "col-md-9 description p-0",
+    class: "col-md-9 p-0",
   });
 
-  // sets css styling compared to current time 
-  var planData = $("<textarea>");
-  hourPlan.append(planData);
-  planData.attr("id", thisHour.id);
+  // sets css styling compared to current time
+  var plannerData = $("<textarea>");
+  hourPlan.append(plannerData);
+  plannerData.attr("id", thisHour.id);
   if (thisHour.time < moment().format("HH")) {
-    planData.attr({
+    plannerData.attr({
       class: "past",
     });
   } else if (thisHour.time === moment().format("HH")) {
-    planData.attr({
+    plannerData.attr({
       class: "present",
     });
   } else if (thisHour.time > moment().format("HH")) {
-    planData.attr({
+    plannerData.attr({
       class: "future",
     });
   }
-  
+
   // creates a save button
   var saveButton = $("<i class='far fa-save fa-lg'></i>");
   var savePlanner = $("<button>").attr({ class: "col-md-1 saveBtn" });
@@ -155,22 +155,17 @@ myDay.forEach(function (thisHour) {
   hourRow.append(hourField, hourPlan, savePlanner);
 });
 
-init();
-
 //save data to local storage
 $(".saveBtn").on("click", function (e) {
   e.preventDefault();
   var saveIndex = $(this)
-  .siblings(".description")
-  .children(".future")
-  .attr("id");
-  myDay[saveIndex].reminder=$(this)
-  .siblings(".description")
-  .children(".future")
-  .val();
-  
-  saveReminders();
-  displayReminders();
+    .siblings(".description")
+    .children(".future")
+    .attr("id");
+  myDay[saveIndex].toDos = $(this)
+    .siblings(".description")
+    .children(".future")
+    .val();
 });
-
-
+init();
+displaytoDos();
